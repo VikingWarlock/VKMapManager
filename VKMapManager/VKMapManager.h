c190942907227ab0a0e3b38b7e409c06ea0fbf1c
VKWK's New MacBook <vikingwarlock@gmail.com>
2016-01-18 23:04:55 +0800

    Initial Commit

---
+++ VKMapManager/VKMapManager.h
@@ -0,0 +1,13 @@
+//
+//  VKMapManager.h
+//  VKMapManager
+//
+//  Created by VKWK on 1/18/16.
+//  Copyright © 2016 VKWK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VKMapManager : NSObject
+
+@end
---
+++ VKMapManager/VKMapManager.m
@@ -0,0 +1,13 @@
+//
+//  VKMapManager.m
+//  VKMapManager
+//
+//  Created by VKWK on 1/18/16.
+//  Copyright © 2016 VKWK. All rights reserved.
+//
+
+#import "VKMapManager.h"
+
+@implementation VKMapManager
+
+@end
