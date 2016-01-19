9f7fe4889c9d46144c33d781526cdc6f8eed8329
VKWK's New MacBook <vikingwarlock@gmail.com>
2016-01-19 11:45:00 +0800

    rename the head file name

--- VKMapManager/VKMapManager.h
+++ VKMapManager/VKMapManager.h
@@ -7,7 +7,32 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@protocol VKMapDelegate <NSObject>
+
+@optional
+
+
+@end
+
 
 @interface VKMapManager : NSObject
+
+@property(nonatomic,readonly,nonnull)UIView *mapview;
+
+@property(nonatomic,weak)id<VKMapDelegate> delegate;
+
+
+
++(BOOL)registerGoogleMapWithAPIKey:(nullable NSString*)apikey;
++(void)registerBaiduMapWithAPIKey:(nullable NSString *)apikey;
+
++(nullable VKMapManager*)sharedObject;
+
+-(void)addPoint:(CLLocationCoordinate2D)coordinate andOptions:(nullable NSDictionary*)options;
+
+-(void)addLines:(nonnull NSArray*)points andOptions:(nullable NSDictionary*)options;
 
 @end
--- VKMapManager/VKMapManager.m
+++ VKMapManager/VKMapManager.m
@@ -7,7 +7,78 @@
 //
 
 #import "VKMapManager.h"
+#import <GoogleMaps/GoogleMaps.h>
+#import <BaiduMapKit/BaiduMapAPI_Map/BMKMapComponent.h>
+
+
+@interface VKMapManager()<BMKGeneralDelegate>
+
+@property(nonatomic,strong)BMKMapManager *bmkManager;
+
+
+@end
 
 @implementation VKMapManager
+
+
+
++(VKMapManager *)sharedObject
+{
+    static id share;
+    static dispatch_once_t once;
+    dispatch_once(&once, ^{
+        if (share==nil) {
+            share=[[VKMapManager alloc]init];
+        }
+    });
+    return share;
+}
+
++(BOOL)registerGoogleMapWithAPIKey:(NSString*)apikey
+{
+    return [GMSServices provideAPIKey:apikey];
+}
+
++(void)registerBaiduMapWithAPIKey:(NSString *)apikey
+{
+    if ([VKMapManager sharedObject].bmkManager==nil) {
+        [VKMapManager sharedObject].bmkManager=[[BMKMapManager alloc]init];
+    }
+    [[VKMapManager sharedObject].bmkManager start:apikey generalDelegate:[VKMapManager sharedObject]];
+}
+
+#pragma Baidu Delegates
+- (void)onGetNetworkState:(int)iError
+{
+    if (0 == iError) {
+        NSLog(@"联网成功");
+    }
+    else{
+        NSLog(@"onGetNetworkState %d",iError);
+    }
+}
+- (void)onGetPermissionState:(int)iError
+{
+    if (0 == iError) {
+        NSLog(@"授权成功");
+    }
+    else {
+        NSLog(@"onGetPermissionState %d",iError);
+    }
+}
+
+#pragma Google Delegates
+
+
+#pragma Application State changed
+-(void)WillResignActive
+{
+    [BMKMapView willBackGround];
+}
+
+-(void)DidBecomeActive
+{
+    [BMKMapView didForeGround];
+}
 
 @end
